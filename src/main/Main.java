6ab76e8ee7fc9fb136c8e71c7f223abb3af5ed0b
rafaScalet <rafaelscalet2018@gmail.com>
2024-04-21 11:51:00 -0300

    smaller and bigger

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,30 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        int bigger = 0;
+        int smaller = 0; 
+        int[] value = new int[10];
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Type 10 numbers: ");
+        for(int indx = 0; indx < value.length; indx++){
+            value[indx] = in.nextInt();
+        }
+        in.close();
+
+        for (int indx = 0; indx < value.length; indx++){
+            if(value[indx] > value[bigger]){
+                bigger = indx;
+            }
+            if(value[indx] < value[smaller]){
+                smaller = indx;
+            }
+        }
+
+        System.out.println("Bigger: " + value[bigger]);
+        System.out.println("Smaller: " + value[smaller]);
     }
 }
